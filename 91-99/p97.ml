277bd977c3b201b1afc4c2dfbd7fdea768eb81e7
Victor Nicollet <victor-github@nicollet.net>
2014-02-11 23:52:51 +0100

    Shorter solution

--- 91-99/p97.ml
+++ 91-99/p97.ml
@@ -28,79 +28,37 @@ Every spot in the puzzle belongs to a (horizontal) row and a (vertical) column, 
 
 *)
 
-exception Cannot_solve
+let solve sudoku =
 
-let fixed b = 
-  let rec collect i j acc =
-    if i > 8 then acc
-    else if j > 8 then collect (i+1) 0 acc
-    else if b.(i).(j) <> 0 then collect i (j+1) ((i,j)::acc)
-    else collect i (j+1) acc
-  in 
-  collect 0 0 []
+  (* Is there an n >= i >= m such that f(i) is true?  *)
+  let rec find m n f = n >= m && (f n || find m (n-1) f) in
 
-let candidates b = 
-  let a = Array.make_matrix 9 9 [] in
-  for i = 0 to 8 do
-    for j = 0 to 8 do
-      if b.(i).(j) = 0 then a.(i).(j) <- []
-      else a.(i).(j) <- [-1]
-    done
-  done;
-  a
+  (* Does value 'v' exist in any line, column or square
+     containing cell (i,j) ? *)
+  let exists v i j =
+    let x  = i - (i mod 3) and y = j - (j mod 3) in
+    find 0 8 (fun k ->
+      sudoku.(i).(k) = v
+      || sudoku.(k).(j) = v
+      || sudoku.(x + (k mod 3)).(y + (k / 3)) = v)
+  in
 
-let digits = [1;2;3;4;5;6;7;8;9]
+  (* Recursive solver. Not tail-recursive, but maximum stack depth is 81.
+     Returns true if solution is found (sudoku then contains that solution). *)
+  let rec solve i j =
+    if i = 9 then
+      j = 8 || solve 0 (j+1)
+    else if sudoku.(i).(j) > 0 then
+      solve (i+1) j
+    else
+      find 1 9 (fun v ->
+        if exists v i j then false else
+          ( sudoku.(i).(j) <- v ;
+            if solve (i+1) j then true else
+              (sudoku.(i).(j) <- 0 ; false )))
+  in
 
-let rm l1 l2 = List.fold_left (fun acc x -> if List.mem x l1 then acc else x::acc) [] l2 |> List.rev
-
-let no_dup l =
-  let sl = List.sort compare l in 
-  List.fold_left (fun (acc, d) x -> if x = d then (acc, d) else (x::acc, x)) ([List.hd sl], List.hd sl) (List.tl sl) |> fst
-
-let bad_digits_row i b = Array.fold_left (fun acc x -> if x <> 0 then x::acc else acc) [] b.(i)
-
-let bad_digits_col j b = 
-  let rec col i acc =
-    if i >= 8 then acc
-    else if b.(i).(j) <> 0 then col (i+1) (b.(i).(j)::acc)
-    else col (i+1) acc
-  in 
-  col 0 []
-
-let bad_digits_mt i j b =
-  let rec collect x y acc =
-    if x < 0 then collect (x+1) y acc
-    else if y < 0 then collect x (y+1) acc
-    else if x > i || x > 8 then acc
-    else if y > j || y > 8 then collect (x+1) (j-2) acc
-    else collect x (y+1) (if b.(i).(j) <> 0 then b.(i).(j)::acc else acc)
-  in 
-  collect (i-2) (j-2) []
-
-let bad_digits i j b = 
-  (bad_digits_row i b) @ (bad_digits_col j b) @ (bad_digits_mt i j b) |> no_dup
-
-let candi_digits i j b = rm (bad_digits i j b) digits
-
-let sudoku b =
-  let fixed_places = fixed b in
-  let rec move x y =
-    if x > 8 then true
-    else if y > 8 then move (x+1) 0
-    else if List.mem (x,y) fixed_places then move x (y+1)
-    else 
-      let r = candi_digits x y b |> try_move x y in
-      if r then r
-      else (b.(x).(y) <- 0;r)
-  and try_move i j = function
-    | [] -> false
-    | hd::tl -> (
-      b.(i).(j) <- hd;
-      if move i (j+1) then true
-      else try_move i j tl
-    )
-  in 
-  move 0 0
+  solve 0 0
 
 (* test *)
 
@@ -118,7 +76,7 @@ let _ =
       [|2;  4;  0;  0;  0;  1;  5;  0;  0|];
     |]
   in
-  if sudoku b then b else raise Cannot_solve
+  if solve b then b else failwith "Cannot solve!"
 
   
 

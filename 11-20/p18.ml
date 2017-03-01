a13e625545d54d4d927b39b18ab1ffba7de6b7e6
Victor Nicollet <victor-github@nicollet.net>
2017-03-01 14:33:45 +0100

    Rework p18
    Improved based on recommendation by @quangminhtran in #9

--- 11-20/p18.ml
+++ 11-20/p18.ml
@@ -2,17 +2,17 @@
 
 let slice list b e = 
 
-  let rec take n = function
+  let rec aux drop take = function 
     | [] -> []
-    | h :: t -> if n = 0 then [] else h :: take (n-1) t
+    | h :: t -> if take = 0 then 
+                  [] 
+                else if drop = 0 then 
+                  h :: aux 0 (take - 1) t
+                else
+                  aux (drop - 1) take t
   in
 
-  let rec drop n = function
-    | [] -> []
-    | h :: t as l -> if n = 0 then l else drop (n-1) t
-  in
-
-  take (e - b + 1) (drop (b - 1) list) 
+  aux (b - 1) (e - b + 1) list
 ;;
 
 assert (slice [`a;`b;`c;`d;`e;`f;`g;`h;`i;`j] 3 7 = [`c;`d;`e;`f;`g]) ;;

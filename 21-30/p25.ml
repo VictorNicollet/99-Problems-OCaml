c0563cf21a9c780bffaf1b0055b6421fb08086e0
Dominik Moehrle <dominik.moehrle@gmail.com>
2018-09-25 12:29:05 +0200

    simplified p25 by using p23

--- 21-30/p25.ml
+++ 21-30/p25.ml
@@ -2,29 +2,10 @@
 
 let random n = 1337 mod n ;;
 
-let rec permutation list = 
-
-  let rec extract acc n = function
-    | [] -> raise Not_found
-    | h :: t -> if n = 0 then h, acc @ t else extract (h::acc) (n-1) t
-  in
-
-  let extract_rand list len = 
-    extract [] (random len) list 
-  in
-
-  let rec aux acc list len = 
-    if len = 0 then acc else
-      let picked, rest = extract_rand list len in 
-      aux (picked :: acc) rest (len-1)
-  in
-
-  let len = List.length list in
-
-  aux [] list len 
+let permutation list =
+    rand_select list (List.length list)
 ;;
 
-
-assert (permutation [`a;`b;`c;`d;`e;`f] = [`b;`d;`a;`e;`c;`f]) ;;
+assert (permutation [`a;`b;`c;`d;`e;`f] = [`f; `c; `b; `e; `d; `a]) ;;
 
 
